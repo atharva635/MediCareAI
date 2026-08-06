e58bdda452418cd206af0b1ac2caa4e31f06f508
Atharva Gupta <atharvagupta00001@gmail.com>
2026-08-06 20:24:28 +0530

    Configure MongoDB Atlas connection

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+console.log("✅ Auth Routes Loaded");
 
 dotenv.config();
 
@@ -8,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({
--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+};
+
+startServer();
